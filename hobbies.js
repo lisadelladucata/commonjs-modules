11ce89dfb782a7141e20c8ec0a42938b59d98159
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-18 00:39:15 +0100

    feat:add-hobbies

---
+++ hobbies.js
@@ -0,0 +1,7 @@
+function myHobbies( hobbyOne, hobbyTwo, hobbyThree){
+    return{
+        hobbies : ['palestra', 'cinema', 'musica']
+    }
+}
+
+module.exports = myHobbies;
--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const myName = require('./names');
 console.log(myName('Lisa', 'Della Ducata'));
+
+
+const myHobbies = require('./hobbies')
+console.log(myHobbies());
